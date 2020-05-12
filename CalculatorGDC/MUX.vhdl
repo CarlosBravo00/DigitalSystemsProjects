812fb85046cec3444afb1a60a84830d423b4f969
Carlos <carlosemiliobravomarquez@gmail.com>
2020-05-12 08:41:34 -0500

    CalculatorGDC

---
+++ CalculatorGDC/MUX.vhdl
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux is 
+    port(
+        x, y : in std_logic_vector(3 downto 0);
+        sel : in std_logic;
+        res : out std_logic_vector(3 downto 0)
+    );
+end entity;
+
+architecture arch of mux is
+begin
+-- USING WHEN
+   res <=  x when (sel = '0') else
+           y when (sel = '1') else
+           null when OTHERS;
+end arch ;
