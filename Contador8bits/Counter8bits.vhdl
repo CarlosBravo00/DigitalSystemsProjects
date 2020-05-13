5cd5b821c93599127397277795a45d33bc57bbd9
Carlos <carlosemiliobravomarquez@gmail.com>
2020-05-12 20:36:32 -0500

    Correccion

--- Contador8bits/Counter8bits.vhdl
+++ Contador8bits/Counter8bits.vhdl
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity counter8bits  is
     Port ( CLK : in  BIT;
-	        ENABLE : in BIT :='1'; -- 0 = ENABLE , 1 = CONTINUE 
+	        HOLD : in BIT :='1'; -- 0 = HOLD , 1 = CONTINUE 
             UD : in BIT;
            Q : out  BIT_VECTOR(7 downto 0));
 end counter8bits;
@@ -22,7 +22,7 @@ begin
     EDEC <=  (QS(0) and (NOT QS(1)) AND (NOT QS(2)) AND QS(3) and (NOT UD))
              OR ((NOT QS(0)) AND (NOT QS(1)) AND (NOT QS(2)) AND (NOT QS(3)) AND UD);
 
-C0 : counter port map(clk,ENABLE,UD,QS(3 downto 0));   
+C0 : counter port map(clk,HOLD,UD,QS(3 downto 0));   
 C1:  counter port map(clk,EDEC,UD,QS(7 downto 4));
  
 Q<= QS;
